4e20df08c6985a94f34ba71523dd0c02e86ef046
Adullave <theemmanuelime@gmail.com>
2022-03-14 22:20:56 +0000

    Added code for alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Print letters of the alphabets
+ *
+ * Description: Prints alphabets in small caps
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+	putchar('\n');
+	return (0);
+}
