e6b24666f1a085e34342b596b3b2ecd071549b7f
Adullave <theemmanuelime@gmail.com>
2022-03-14 21:06:08 +0000

    Added code for printing -,+ and 0 numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-
 /**
- * * main - determines if number is positive, negative or zero
- * *
- * * Description: The program assigns random numbers to variable n
- * *
- * * Return: Always 0 (Success)
- * */
+* main - determines if number is positive, negative or zero
+*
+* Description: The program assigns random numbers to variable
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0) {
-	printf("%d is negative", n);
-}
-else if (n == 0) {
-	printf("%d is 0", n);
-}
-else {
-	printf("%d is positive", n);
-}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is 0", n);
+	}
+	else
+	{
+		printf("%d is positive", n);
+	}
 	return (0);
 }
-
