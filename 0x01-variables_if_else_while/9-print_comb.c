9ddb863b7f60a971d68ce5d4f5eb014e7b421207
Adullave <99490820+Adullave@users.noreply.github.com>
2022-03-18 12:18:12 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		putchar(58);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
