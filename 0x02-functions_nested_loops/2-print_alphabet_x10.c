cc8b004381cabcee314cfdb08546e7b62d924df4
Adullave <99490820+Adullave@users.noreply.github.com>
2022-03-20 20:35:16 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - print_alphabet_x10 - prints alphabet letters
+* ten times
+* Description: : The function uses loop to operate
+*Return: 0
+*/
+
+int main()
+{
+  int count, a;
+    for (count = 1; count <= 10; count++)
+    {
+       for (a = 'a'; a <= 'z'; a++)
+          _putchar("%c", a);
+        _putchar("\n");
+    }
+}
