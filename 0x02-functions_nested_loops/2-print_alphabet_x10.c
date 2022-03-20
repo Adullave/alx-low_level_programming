b0a828774dc1221c3ac48c06b7be1f086af42a19
Adullave <99490820+Adullave@users.noreply.github.com>
2022-03-20 20:47:19 +0100

    A function that checks for lowercase character

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-* main - print_alphabet_x10 - prints alphabet letters
-* ten times
+* main - print_alphabet_x10 - prints alphabet letters ten times
 * Description: The function uses loop to operate
-*Return: 0
+*Return: Always 0 (Success)
 */
 
 void print_alphabet_x10(void)
